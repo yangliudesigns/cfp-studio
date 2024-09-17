bc2f34420347cc17967b06080e481408751aa04f
yangliudesigns <yangliudesigns@gmail.com>
2024-09-17 19:07:13 -0400

    Add more website structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Gallery from "./pages/Gallery";
+import Pricing from "./pages/Pricing";
+
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
+    </Router>
   )
 }
 
