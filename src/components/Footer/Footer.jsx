2600d3b7f5f0864b5fa8383acb41c764f92e83a6
yangliudesigns <yangliudesigns@gmail.com>
2024-09-18 16:26:47 -0400

    Implement footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import "./Footer.scss";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>© 2024 CharFaerie Production Inc. All Rights Reserved.
+      </p>
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 
 export default function Home() {
   return (
     <div>
       <Header />
-      {/* <h1>Welcome to Home</h1> */}
+      <Footer />
     </div>
   )
 }
