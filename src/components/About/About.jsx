5b0bcc230d5c775207c1cf7443352b465f8f7f6c
yangliudesigns <yangliudesigns@gmail.com>
2024-09-19 11:10:53 -0400

    Implement about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import "./About.scss";
+
 
 export default function About() {
   return (
-    <div>
-      
+    <div className="about">
+      <h4>
+      Photography holds the power to bring back moments once lived and your stories. 
+      </h4>
+      <p>
+      It captures the essence of people's lives, creating images that will make you smile or cry years later.
+      </p>
+      <p>
+        CharFaerie
+      </p>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
+import About from "../components/About/About";
 import Footer from "../components/Footer/Footer";
 
 
@@ -8,6 +9,7 @@ export default function Home() {
     <div>
       <Header />
       <Hero />
+      <About />
       <Footer />
     </div>
   )
