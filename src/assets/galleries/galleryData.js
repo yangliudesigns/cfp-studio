1ae30fe5757aa49cede1fc323528d781fddd2586
yangliudesigns <yangliudesigns@gmail.com>
2024-09-22 20:11:21 -0400

    Add gallery new images data

---
+++ src/assets/galleries/galleryData.js
@@ -0,0 +1,218 @@
+import gallery1 from "./001.webp";
+import gallery2 from "./002.webp";
+import gallery3 from "./003.webp";
+import gallery4 from "./004.webp";
+import gallery5 from "./005.webp";
+import gallery6 from "./006.webp";
+import gallery7 from "./007.webp";
+import gallery8 from "./008.webp";
+import gallery9 from "./009.webp";
+import gallery10 from "./010.webp";
+import gallery11 from "./011.webp";
+import gallery12 from "./012.webp";
+import gallery13 from "./013.webp";
+import gallery14 from "./014.webp";
+import gallery15 from "./015.webp";
+import gallery16 from "./016.webp";
+import gallery17 from "./017.webp";
+import gallery18 from "./018.webp";
+import gallery19 from "./019.webp";
+import gallery20 from "./020.webp";
+import gallery21 from "./021.webp";
+import gallery22 from "./022.webp";
+import gallery23 from "./023.webp";
+import gallery24 from "./024.webp";
+import gallery25 from "./025.webp";
+import gallery26 from "./026.webp";
+import gallery27 from "./027.webp";
+import gallery28 from "./028.webp";
+import gallery29 from "./029.webp";
+import gallery30 from "./030.webp";
+import gallery31 from "./031.webp";
+import gallery32 from "./032.webp";
+import gallery33 from "./033.webp";
+import gallery34 from "./034.webp";
+import gallery35 from "./035.webp";
+import gallery36 from "./036.webp";
+import gallery37 from "./037.webp";
+import gallery38 from "./038.webp";
+import gallery39 from "./039.webp";
+import gallery40 from "./040.webp";
+import gallery41 from "./041.webp";
+import gallery42 from "./042.webp";
+import gallery43 from "./043.webp";
+import gallery44 from "./044.webp";
+import gallery45 from "./045.webp";
+import gallery46 from "./046.webp";
+import gallery47 from "./047.webp";
+import gallery48 from "./048.webp";
+import gallery49 from "./049.webp";
+import gallery50 from "./050.webp";
+import gallery51 from "./051.webp";
+import gallery52 from "./052.webp";
+import gallery53 from "./053.webp";
+import gallery54 from "./054.webp";
+import gallery55 from "./055.webp";
+import gallery56 from "./056.webp";
+import gallery57 from "./057.webp";
+import gallery58 from "./058.webp";
+import gallery59 from "./059.webp";
+import gallery60 from "./060.webp";
+import gallery61 from "./061.webp";
+import gallery62 from "./062.webp";
+import gallery63 from "./063.webp";
+import gallery64 from "./064.webp";
+import gallery65 from "./065.webp";
+import gallery66 from "./066.webp";
+import gallery67 from "./067.webp";
+import gallery68 from "./068.webp";
+import gallery69 from "./069.webp";
+import gallery70 from "./070.webp";
+import gallery71 from "./071.webp";
+import gallery72 from "./072.webp";
+import gallery73 from "./073.webp";
+import gallery74 from "./074.webp";
+import gallery75 from "./075.webp";
+import gallery76 from "./076.webp";
+import gallery77 from "./077.webp";
+import gallery78 from "./078.webp";
+import gallery79 from "./079.webp";
+import gallery80 from "./080.webp";
+import gallery81 from "./081.webp";
+import gallery82 from "./082.webp";
+import gallery83 from "./083.webp";
+import gallery84 from "./084.webp";
+import gallery85 from "./085.webp";
+import gallery86 from "./086.webp";
+import gallery87 from "./087.webp";
+import gallery88 from "./088.webp";
+import gallery89 from "./089.webp";
+import gallery90 from "./090.webp";
+import gallery91 from "./091.webp";
+import gallery92 from "./092.webp";
+import gallery93 from "./093.webp";
+import gallery94 from "./094.webp";
+import gallery95 from "./095.webp";
+import gallery96 from "./096.webp";
+import gallery97 from "./097.webp";
+import gallery98 from "./098.webp";
+import gallery99 from "./099.webp";
+import gallery100 from "./100.webp";
+import gallery101 from "./101.webp";
+import gallery102 from "./102.webp";
+import gallery103 from "./103.webp";
+import gallery104 from "./104.webp";
+import gallery105 from "./105.webp";
+import gallery106 from "./106.webp";
+import gallery107 from "./107.webp";
+
+// 将图片导出为数组
+export const galleryData = [
+  { id: 1, src: gallery1, alt: "Gallery Image 1" },
+  { id: 2, src: gallery2, alt: "Gallery Image 2" },
+  { id: 3, src: gallery3, alt: "Gallery Image 3" },
+  { id: 4, src: gallery4, alt: "Gallery Image 4" },
+  { id: 5, src: gallery5, alt: "Gallery Image 5" },
+  { id: 6, src: gallery6, alt: "Gallery Image 6" },
+  { id: 7, src: gallery7, alt: "Gallery Image 7" },
+  { id: 8, src: gallery8, alt: "Gallery Image 8" },
+  { id: 9, src: gallery9, alt: "Gallery Image 9" },
+  { id: 10, src: gallery10, alt: "Gallery Image 10" },
+  { id: 11, src: gallery11, alt: "Gallery Image 11" },
+  { id: 12, src: gallery12, alt: "Gallery Image 12" },
+  { id: 13, src: gallery13, alt: "Gallery Image 13" },
+  { id: 14, src: gallery14, alt: "Gallery Image 14" },
+  { id: 15, src: gallery15, alt: "Gallery Image 15" },
+  { id: 16, src: gallery16, alt: "Gallery Image 16" },
+  { id: 17, src: gallery17, alt: "Gallery Image 17" },
+  { id: 18, src: gallery18, alt: "Gallery Image 18" },
+  { id: 19, src: gallery19, alt: "Gallery Image 19" },
+  { id: 20, src: gallery20, alt: "Gallery Image 20" },
+  { id: 21, src: gallery21, alt: "Gallery Image 21" },
+  { id: 22, src: gallery22, alt: "Gallery Image 22" },
+  { id: 23, src: gallery23, alt: "Gallery Image 23" },
+  { id: 24, src: gallery24, alt: "Gallery Image 24" },
+  { id: 25, src: gallery25, alt: "Gallery Image 25" },
+  { id: 26, src: gallery26, alt: "Gallery Image 26" },
+  { id: 27, src: gallery27, alt: "Gallery Image 27" },
+  { id: 28, src: gallery28, alt: "Gallery Image 28" },
+  { id: 29, src: gallery29, alt: "Gallery Image 29" },
+  { id: 30, src: gallery30, alt: "Gallery Image 30" },
+  { id: 31, src: gallery31, alt: "Gallery Image 31" },
+  { id: 32, src: gallery32, alt: "Gallery Image 32" },
+  { id: 33, src: gallery33, alt: "Gallery Image 33" },
+  { id: 34, src: gallery34, alt: "Gallery Image 34" },
+  { id: 35, src: gallery35, alt: "Gallery Image 35" },
+  { id: 36, src: gallery36, alt: "Gallery Image 36" },
+  { id: 37, src: gallery37, alt: "Gallery Image 37" },
+  { id: 38, src: gallery38, alt: "Gallery Image 38" },
+  { id: 39, src: gallery39, alt: "Gallery Image 39" },
+  { id: 40, src: gallery40, alt: "Gallery Image 40" },
+  { id: 41, src: gallery41, alt: "Gallery Image 41" },
+  { id: 42, src: gallery42, alt: "Gallery Image 42" },
+  { id: 43, src: gallery43, alt: "Gallery Image 43" },
+  { id: 44, src: gallery44, alt: "Gallery Image 44" },
+  { id: 45, src: gallery45, alt: "Gallery Image 45" },
+  { id: 46, src: gallery46, alt: "Gallery Image 46" },
+  { id: 47, src: gallery47, alt: "Gallery Image 47" },
+  { id: 48, src: gallery48, alt: "Gallery Image 48" },
+  { id: 49, src: gallery49, alt: "Gallery Image 49" },
+  { id: 50, src: gallery50, alt: "Gallery Image 50" },
+  { id: 51, src: gallery51, alt: "Gallery Image 51" },
+  { id: 52, src: gallery52, alt: "Gallery Image 52" },
+  { id: 53, src: gallery53, alt: "Gallery Image 53" },
+  { id: 54, src: gallery54, alt: "Gallery Image 54" },
+  { id: 55, src: gallery55, alt: "Gallery Image 55" },
+  { id: 56, src: gallery56, alt: "Gallery Image 56" },
+  { id: 57, src: gallery57, alt: "Gallery Image 57" },
+  { id: 58, src: gallery58, alt: "Gallery Image 58" },
+  { id: 59, src: gallery59, alt: "Gallery Image 59" },
+  { id: 60, src: gallery60, alt: "Gallery Image 60" },
+  { id: 61, src: gallery61, alt: "Gallery Image 61" },
+  { id: 62, src: gallery62, alt: "Gallery Image 62" },
+  { id: 63, src: gallery63, alt: "Gallery Image 63" },
+  { id: 64, src: gallery64, alt: "Gallery Image 64" },
+  { id: 65, src: gallery65, alt: "Gallery Image 65" },
+  { id: 66, src: gallery66, alt: "Gallery Image 66" },
+  { id: 67, src: gallery67, alt: "Gallery Image 67" },
+  { id: 68, src: gallery68, alt: "Gallery Image 68" },
+  { id: 69, src: gallery69, alt: "Gallery Image 69" },
+  { id: 70, src: gallery70, alt: "Gallery Image 70" },
+  { id: 71, src: gallery71, alt: "Gallery Image 71" },
+  { id: 72, src: gallery72, alt: "Gallery Image 72" },
+  { id: 73, src: gallery73, alt: "Gallery Image 73" },
+  { id: 74, src: gallery74, alt: "Gallery Image 74" },
+  { id: 75, src: gallery75, alt: "Gallery Image 75" },
+  { id: 76, src: gallery76, alt: "Gallery Image 76" },
+  { id: 77, src: gallery77, alt: "Gallery Image 77" },
+  { id: 78, src: gallery78, alt: "Gallery Image 78" },
+  { id: 79, src: gallery79, alt: "Gallery Image 79" },
+  { id: 80, src: gallery80, alt: "Gallery Image 80" },
+  { id: 81, src: gallery81, alt: "Gallery Image 81" },
+  { id: 82, src: gallery82, alt: "Gallery Image 82" },
+  { id: 83, src: gallery83, alt: "Gallery Image 83" },
+  { id: 84, src: gallery84, alt: "Gallery Image 84" },
+  { id: 85, src: gallery85, alt: "Gallery Image 85" },
+  { id: 86, src: gallery86, alt: "Gallery Image 86" },
+  { id: 87, src: gallery87, alt: "Gallery Image 87" },
+  { id: 88, src: gallery88, alt: "Gallery Image 88" },
+  { id: 89, src: gallery89, alt: "Gallery Image 89" },
+  { id: 90, src: gallery90, alt: "Gallery Image 90" },
+  { id: 91, src: gallery91, alt: "Gallery Image 91" },
+  { id: 92, src: gallery92, alt: "Gallery Image 92" },
+  { id: 93, src: gallery93, alt: "Gallery Image 93" },
+  { id: 94, src: gallery94, alt: "Gallery Image 94" },
+  { id: 95, src: gallery95, alt: "Gallery Image 95" },
+  { id: 96, src: gallery96, alt: "Gallery Image 96" },
+  { id: 97, src: gallery97, alt: "Gallery Image 97" },
+  { id: 98, src: gallery98, alt: "Gallery Image 98" },
+  { id: 99, src: gallery99, alt: "Gallery Image 99" },
+  { id: 100, src: gallery100, alt: "Gallery Image 100" },
+  { id: 101, src: gallery101, alt: "Gallery Image 101" },
+  { id: 102, src: gallery102, alt: "Gallery Image 102" },
+  { id: 103, src: gallery103, alt: "Gallery Image 103" },
+  { id: 104, src: gallery104, alt: "Gallery Image 104" },
+  { id: 105, src: gallery105, alt: "Gallery Image 105" },
+  { id: 106, src: gallery106, alt: "Gallery Image 106" },
+  { id: 107, src: gallery107, alt: "Gallery Image 107" },
+];
