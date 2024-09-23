7f060fcd8d9bfec2b902b102479f875756570683
yangliudesigns <yangliudesigns@gmail.com>
2024-09-22 22:59:06 -0400

    Update price card and global style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.scss";
 export default function Footer() {
   return (
     <div className="footer">
-      <p>© 2024 CharFaerie Production Inc. All Rights Reserved.
+      <p className="footer__text">© 2024 CharFaerie Production Inc. All Rights Reserved.
       </p>
     </div>
   )
--- src/components/PriceCard/PriceCard.jsx
+++ src/components/PriceCard/PriceCard.jsx
@@ -5,7 +5,7 @@ export default function PricingCard({ logoSrc, location, description, price }) {
     return (
       <div className="pricing-card">
         <div className="pricing-card__overlay">
-          <img src={logoSrc} alt="Logo" className="pricing-card__logo" />
+          <img src={logoSrc} alt="sub-branding logo" className="pricing-card__logo" />
           <p className="pricing-card__location">{location}</p>
           <p className="pricing-card__description">{description}</p>
           <p className="pricing-card__price">From ${price}</p>
