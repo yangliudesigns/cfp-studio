b956d312b31844321defea19bf26dcb33110861d
yangliudesigns <yangliudesigns@gmail.com>
2024-09-22 23:58:38 -0400

    Fix pricing card and pricing car list

--- src/components/PriceCardList/PriceCardList.jsx
+++ src/components/PriceCardList/PriceCardList.jsx
@@ -1,7 +1,7 @@
 import "./PriceCardList.scss";
 import PricingCard from "../PriceCard/PriceCard";
 import React from 'react';
-import pricingData from '../../data/price.json';  // 直接 import JSON 数据
+import pricingData from '../../data/price.json';  
 import logo1 from "../../assets/images/CHARL.png";
 import logo2 from "../../assets/images/FAERIE.png";
 import logo3 from "../../assets/images/LIGHT.png";
@@ -19,7 +19,7 @@ export default function PricingCardList() {
       {pricingData.map((item) => (
         <PricingCard
           key={item.id}
-          logoSrc={logoMap[item.logoSrc]}  // 根据 JSON 中的 logoSrc 动态获取图片
+          logoSrc={logoMap[item.logoSrc]}  
           location={item.location}
           description={item.description}
           price={item.price}
