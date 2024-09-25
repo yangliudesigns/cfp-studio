78b37ce2968d7be98d0ea1cf279427e9fcfb8c73
yangliudesigns <yangliudesigns@gmail.com>
2024-09-25 16:25:23 -0400

    Fix garllery typo

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,7 +2,7 @@ import Header from "../components/Header/Header";
 import GalleryGrid from "../components/GalleryGrid/GalleryGrid";
 import Footer from "../components/Footer/Footer";
 import HerosForPages from "../components/HerosForPages/HerosForPages";
-import GalleryHero from "../assets/images/hero-gallery.jpg";
+import GalleryHero from "../assets/images/hero-galleries.jpg";
 
 
 export default function Gallery() {
